08f7884fcebdb022f043c2144945d50103e3ec3a
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-08-28 14:50:53 +0700

    fix menu

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
                 {
                     path: '/',
                     name: 'dashboard',
-                    component: () => import('../views/pages/home.vue'),
+                    component: () => import('../views/pages/Home.vue'),
                 },
                 {
                     path: '/users',
@@ -44,62 +44,7 @@ const router = createRouter({
                     name: 'status',
                     component: () => import('../views/pages/Status.vue'),
                 },
-                {
-                    path: '/uikit/formlayout',
-                    name: 'formlayout',
-                    component: () => import('../views/uikit/FormLayout.vue'),
-                },
-                {
-                    path: '/uikit/input',
-                    name: 'input',
-                    component: () => import('../views/uikit/Input.vue'),
-                },
-                {
-                    path: '/uikit/floatlabel',
-                    name: 'floatlabel',
-                    component: () => import('../views/uikit/FloatLabel.vue'),
-                },
-                {
-                    path: '/uikit/invalidstate',
-                    name: 'invalidstate',
-                    component: () => import('../views/uikit/InvalidState.vue'),
-                },
-                {
-                    path: '/uikit/button',
-                    name: 'button',
-                    component: () => import('../views/uikit/Button.vue'),
-                },
-                {
-                    path: '/uikit/table',
-                    name: 'table',
-                    component: () => import('../views/uikit/Table.vue'),
-                },
-                {
-                    path: '/uikit/list',
-                    name: 'list',
-                    component: () => import('../views/uikit/List.vue'),
-                },
-                {
-                    path: '/uikit/tree',
-                    name: 'tree',
-                    component: () => import('../views/uikit/Tree.vue'),
-                },
-                {
-                    path: '/uikit/panel',
-                    name: 'panel',
-                    component: () => import('../views/uikit/Panels.vue'),
-                },
-
-                {
-                    path: '/uikit/overlay',
-                    name: 'overlay',
-                    component: () => import('../views/uikit/Overlay.vue'),
-                },
-                {
-                    path: '/uikit/media',
-                    name: 'media',
-                    component: () => import('../views/uikit/Media.vue'),
-                },
+                
             ],
         },
         {
